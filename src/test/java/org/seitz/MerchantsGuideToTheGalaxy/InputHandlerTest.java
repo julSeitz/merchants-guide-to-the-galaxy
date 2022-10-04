00ac7584a0fd866709c5f933f06416467fd88f78
Seitz <jpseitz@online.de>
2022-10-04 21:08:21 +0200

    Added set up for tests

---
+++ src/test/java/org/seitz/MerchantsGuideToTheGalaxy/InputHandlerTest.java
@@ -0,0 +1,15 @@
+package org.seitz.MerchantsGuideToTheGalaxy;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputHandlerTest {
+
+    InputHandler handler;
+
+    @BeforeEach
+    void setUp() {
+        handler = new InputHandler();
+    }
+}
