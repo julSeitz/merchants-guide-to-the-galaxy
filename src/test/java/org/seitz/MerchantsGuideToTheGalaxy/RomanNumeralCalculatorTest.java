e00869584bc3ef048e66f048c3a478ded8018a6f
Seitz <jpseitz@online.de>
2022-10-04 18:22:38 +0200

    Added creation of RomanNumeralCalculator object for tests

--- src/test/java/org/seitz/MerchantsGuideToTheGalaxy/RomanNumeralCalculatorTest.java
+++ src/test/java/org/seitz/MerchantsGuideToTheGalaxy/RomanNumeralCalculatorTest.java
@@ -1,8 +1,15 @@
 package org.seitz.MerchantsGuideToTheGalaxy;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumeralCalculatorTest {
 
+    RomanNumeralCalculator calculator;
 
+    @BeforeEach
+    void setUp() {
+        calculator = new RomanNumeralCalculator();
+    }
 }
