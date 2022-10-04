6eebe16b778135f07dba4a708399ccf6d2d43d0e
Seitz <jpseitz@online.de>
2022-10-04 18:30:02 +0200

    Added test factory for checking if symbols are repeated 4 times

--- src/test/java/org/seitz/MerchantsGuideToTheGalaxy/RomanNumeralCalculatorTest.java
+++ src/test/java/org/seitz/MerchantsGuideToTheGalaxy/RomanNumeralCalculatorTest.java
@@ -1,6 +1,12 @@
 package org.seitz.MerchantsGuideToTheGalaxy;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,4 +18,22 @@ class RomanNumeralCalculatorTest {
     void setUp() {
         calculator = new RomanNumeralCalculator();
     }
+
+    //-----------------------------------START OF TESTS FOR isValidNumeral()--------------------------------------------
+
+    // Testing if "I", "X", "C" or "M" can only be repeated up to 3 times in succession
+
+    @TestFactory
+    Stream<DynamicTest> shouldNotBeAbleToRepeatSymbolFourTimes() {
+        RomanNumeralCalculator secondCalculator = new RomanNumeralCalculator();
+        List<String> inputNumerals = Arrays.asList("I", "X", "C", "M");
+
+        return inputNumerals.stream().map(
+                numeral -> DynamicTest.dynamicTest(numeral + " can not be repeated 4 times",
+                        () -> {
+                            assertFalse(secondCalculator.isValidNumeral(numeral.repeat(4)));
+                        }));
+    }
+
+    //-----------------------------------END OF TESTS FOR isValidNumeral()----------------------------------------------
 }
