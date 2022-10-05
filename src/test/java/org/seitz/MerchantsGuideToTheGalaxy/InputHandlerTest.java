299b82fa760420cae7b8ba0140c5ce87b9c36d9a
Seitz <jpseitz@online.de>
2022-10-05 15:39:25 +0200

    Added test factory to check if parseQuery() returns correct answer

--- src/test/java/org/seitz/MerchantsGuideToTheGalaxy/InputHandlerTest.java
+++ src/test/java/org/seitz/MerchantsGuideToTheGalaxy/InputHandlerTest.java
@@ -308,6 +308,50 @@ class InputHandlerTest {
         );
     }
 
+    //--------------------------------------END OF TESTS FOR extractDataFromQuery()-------------------------------------
 
+    //-----------------------------------------START OF TESTS FOR parseQuery()------------------------------------------
+
+    @TestFactory
+    Stream<DynamicTest> isQueryParsedCorrectly() {
+        InputHandler secondHandler = new InputHandler();
+
+        List<String> inputQueries = Arrays.asList(
+                "glob is I",
+                "prok is V",
+                "pish is X",
+                "tegj is L",
+                "glob glob Silver is 34 Credits",
+                "glob prok Gold is 57800 Credits",
+                "pish pish Iron is 3910 Credits",
+                "how much is pish tegj glob glob ?",
+                "how many Credits is glob prok Silver ?",
+                "how many Credits is glob prok Gold ?",
+                "how many Credits is glob prok Iron ?",
+                "how much wood could a woodchuck chuck if a woodchuck could chuck wood ?"
+        );
+
+        List<String> answers = Arrays.asList(
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "pish tegj glob glob is 42",
+                "glob prok Silver is 68 Credits",
+                "glob prok Gold is 57800 Credits",
+                "glob prok Iron is 782 Credits",
+                "I have no idea what you are talking about"
+        );
+
+        return inputQueries.stream().map(
+                query -> DynamicTest.dynamicTest("Is answer to query " + query + " correct?",
+                        () -> assertEquals(answers.get(inputQueries.indexOf(query)),
+                                secondHandler.parseQuery(query))
+                )
+        );
+    }
 
 }
