802fc5c66055f1984f7bffe43083015c836c4fbb
Seitz <jpseitz@online.de>
2022-10-06 14:22:09 +0200

    Added test to check if user input not equaling "EXIT" is recognized

--- src/test/java/org/seitz/MerchantsGuideToTheGalaxy/IOHandlerTest.java
+++ src/test/java/org/seitz/MerchantsGuideToTheGalaxy/IOHandlerTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -67,5 +69,17 @@ class IOHandlerTest {
         parser.parseQuery("glob prok Gold is 57800 Credits");
         handler.outputAnswerToQuery("how many Credits is glob prok Gold ?", parser);
         assertEquals("> glob prok Gold is 57800 Credits\n", consoleOut.toString());
+    }
+
+    //----------------------------------END OF TESTS FOR outputAnswerToQuery()------------------------------------------
+
+    //---------------------------------------START OF TESTS FOR readAndWrite()------------------------------------------
+
+    @Test
+    void shouldReturnFalseWhenUserInputIsNotExit() {
+        ByteArrayInputStream in = new ByteArrayInputStream("something".getBytes());
+        System.setIn(in);
+        Scanner scanner = new Scanner(in);
+        assertFalse(handler.readAndWrite(scanner, parser));
     }
 }
