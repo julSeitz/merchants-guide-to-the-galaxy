4f848b18ea49f3125c0819f350184f541d79a181
Seitz <jpseitz@online.de>
2022-10-06 14:29:45 +0200

    Implemented main(). Working release.

--- src/main/java/org/seitz/MerchantsGuideToTheGalaxy/Main.java
+++ src/main/java/org/seitz/MerchantsGuideToTheGalaxy/Main.java
@@ -1,7 +1,16 @@
 package org.seitz.MerchantsGuideToTheGalaxy;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IOHandler handler = new IOHandler();
+        InputParser parser = new InputParser();
+        boolean cont = true;
+
+        while (cont) {
+            Scanner scanner = new Scanner(System.in);
+            cont = handler.readAndWrite(scanner, parser);
+        }
     }
 }
