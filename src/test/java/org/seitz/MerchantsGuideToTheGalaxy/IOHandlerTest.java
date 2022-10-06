ff6d37ebfbbc5273a418870c3f5b64a5a64d06a0
Seitz <jpseitz@online.de>
2022-10-06 12:58:04 +0200

    Added test to check if no answer is printed, when numeral is set

--- src/test/java/org/seitz/MerchantsGuideToTheGalaxy/IOHandlerTest.java
+++ src/test/java/org/seitz/MerchantsGuideToTheGalaxy/IOHandlerTest.java
@@ -1,4 +1,38 @@
 package org.seitz.MerchantsGuideToTheGalaxy;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class IOHandlerTest {
+
+    private final ByteArrayOutputStream consoleOut = new ByteArrayOutputStream();
+
+    private final PrintStream originalOut = System.out;
+
+    IOHandler handler;
+
+    @BeforeEach
+    public void setUp() {
+        handler = new IOHandler();
+        System.setOut(new PrintStream(consoleOut));
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        System.setOut(originalOut);
+    }
+
+    //--------------------------------START OF TESTS FOR outputAnswerToQuery()------------------------------------------
+
+    @Test
+    void shouldPrintEmptyLineWhenNumeralIsSet() {
+        handler.outputAnswerToQuery("glob is I");
+        assertEquals("", consoleOut.toString());
+    }
 }
